3233c24f46a46a706ecc547deed9c0eae7e3b55d
odyvic16-dotcom <odyvic16@gmail.com>
2026-09-15 13:46:14 +0100

    Serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('reservations/', include('reservations.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
