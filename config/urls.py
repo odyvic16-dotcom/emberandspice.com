27b687d7977027d5019befdf40f33a6d9e30b5d6
odyvic16-dotcom <odyvic16@gmail.com>
2026-09-15 13:54:34 +0100

    Fix media file serving in production

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('menu/', include('menu.urls')),
     path('reservations/', include('reservations.urls')),
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
